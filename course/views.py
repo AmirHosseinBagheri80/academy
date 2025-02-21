9013336a54e130daf4540edc55a53d4540689113
AHB <amirhossein.b.cr7@gmail.com>
2025-02-22 01:33:11 +0330

    updating courses list

--- course/views.py
+++ course/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 courses=[
-    {"id":1,"name":"python","teacher":"AR"}
+    {"id":1,"name":"python","teacher":"AR"},
+    {"id":2,"name":"django","teacher":"AR"}
 ]
 def course_list(request):
     data=""
