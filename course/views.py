c1320adf9bc8b3b9569deeb5352ea481d19624a8
AHB <amirhossein.b.cr7@gmail.com>
2025-02-21 22:49:28 +0330

    showing courses-on intro branch

--- course/views.py
+++ course/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+courses=[
+    {"id":1,"name":"python","teacher":"AR"}
+]
+def course_list(request):
+    data=""
+    for item in courses:
+        data=data+f"id={item['id']},name={item['name']},teacher={item['teacher']}"+"<br>"
+    return HttpResponse(data)
